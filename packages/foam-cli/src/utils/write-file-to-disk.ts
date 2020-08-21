c0c8a2ad7fc26bcff76846794bff05ad4851827c
Vishesh Handa <me@vhanda.in>
2020-08-21 12:29:41 +0200

    Remove the code
    I just want the docs in order to test GitJournal's Graph Visualization.

--- packages/foam-cli/src/utils/write-file-to-disk.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'fs';
-
-export const writeFileToDisk = async (fileUri: string, data: string) => {
-  return fs.promises.writeFile(fileUri, data);
-};
--- packages/foam-vscode/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { ExtensionContext } from "vscode";
-import { Foam } from "foam-core";
-
-export interface FoamFeature {
-  activate: (context: ExtensionContext, foamPromise: Promise<Foam>) => void
-}
